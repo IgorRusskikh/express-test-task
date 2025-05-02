d280e5928eefc5f1d90dd3c208c13c8c4d3e9938
IgorRusskikh <russkikh200@mail.ru>
2025-05-03 02:17:24 +0700

    feat: add middleware to handle errors

---
+++ src/middleware/error.middleware.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from 'express';
+
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  console.error(err.stack);
+  res.status(500).json({
+    message: 'Internal server error',
+    error: process.env.NODE_ENV === 'production' ? {} : err,
+  });
+};
+
+export const asyncHandler =
+  (fn: Function) => (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
